210ca23c5219fe5099413618668836b1ee373aab
tomason182 <elotro182@hotmail.com>
2024-06-20 12:44:45 -0300

    add interactivity to Card component

---
+++ src/components/Card.jsx
@@ -0,0 +1,21 @@
+import { useState } from "react";
+
+export default function Card(card) {
+  const [wasClicked, setWasClicked] = false;
+
+  return (
+    <div
+      className={`card ${wasClicked && "clicked"}`}
+      onClick={() => {
+        setWasClicked(!wasClicked);
+      }}
+    >
+      <div className="image">
+        <img src={card.imageUrl} alt={card.title} />
+      </div>
+      <div className="description">
+        <span>{card.title}</span>
+      </div>
+    </div>
+  );
+}
