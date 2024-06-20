12ffb84aae0db0765ca550626656933918b78f28
tomason182 <elotro182@hotmail.com>
2024-06-20 19:19:13 -0300

    fix App component

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,11 @@ import Content from "./components/Content";
 export default function App() {
   const [score, setScore] = useState(0);
   const [bestScore, setBestScore] = useState(0);
-  const [wasClicked, setWasClicked] = useState(false);
 
   return (
     <>
       <Header score={score} bestScore={bestScore} />
-      <Content wasClicked={wasClicked} setWasClicked={setWasClicked} />
+      <Content setScore={setScore} bestScore={setBestScore} />
     </>
   );
 }
