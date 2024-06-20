a5456bb6df761d30c10da8205c2e64202f988e4a
tomason182 <elotro182@hotmail.com>
2024-06-20 11:29:48 -0300

    add header basic style
    Use css-module to add style to Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,8 @@
+import styles from "./Header.module.css";
+
 export default function Header() {
   return (
-    <div id="header">
+    <div id={styles.header}>
       <div className="left-section">
         <h1>Game Name</h1>
         <p>Game description - How to play</p>
