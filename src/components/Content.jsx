82fa627f94c4851ebe78422b014eaf682350f23b
tomason182 <elotro182@hotmail.com>
2024-06-20 18:14:37 -0300

    add: randomize characters position

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,13 +1,16 @@
 import BobsBurgerApi from "../api/BobsBurgerApi";
+import style from "./Content.module.css";
 
 export default function Content() {
   const characters = [...Array(10).keys()].map((i) => i + 1);
 
+  const randomCharacters = characters.sort((a, b) => 0.5 - Math.random());
+
   return (
-    <>
-      {characters.map((character) => (
+    <div className={style.content}>
+      {randomCharacters.map((character) => (
         <BobsBurgerApi key={character} character={character} />
       ))}
-    </>
+    </div>
   );
 }
