66516f0f90ad61364af610c2fcb9a7e8ac7ba94a
tomason182 <elotro182@hotmail.com>
2024-06-20 15:21:37 -0300

    add BobsBurgerApi component

--- src/api/BobsBurgerApi.jsx
+++ src/api/BobsBurgerApi.jsx
@@ -1,3 +1,30 @@
+import { useEffect, useState } from "react";
+import styles from "./BobsBurgerApi.module.css";
+
 export default function BobsBurgerApi() {
-  return;
+  const [imageUrl, setImageUrl] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch("https://bobsburgers-api.herokuapp.com/characters/1", {
+      mode: "cors",
+    })
+      .then((response) => {
+        if (response.status >= 400) {
+          throw new Error("Server Error");
+        }
+
+        return response.json();
+      })
+      .then((data) => setImageUrl(data.image))
+      .catch((err) => setError(err))
+      .finally(() => setLoading(false));
+  }, []);
+
+  if (error) return <p>A Network error ocurred</p>;
+  if (loading) return <p>Loading Images...</p>;
+  return (
+    <>{imageUrl && <img className={styles.img} src={imageUrl} alt="" />}</>
+  );
 }
--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,3 +1,9 @@
+import BobsBurgerApi from "../api/BobsBurgerApi";
+
 export default function Content() {
-  return <div>This is the main Content</div>;
+  return (
+    <>
+      <BobsBurgerApi />
+    </>
+  );
 }
