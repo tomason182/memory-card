53752bd3edd5a62f8c2380e140612e5984cb5e86
tomason182 <elotro182@hotmail.com>
2024-06-20 11:01:13 -0300

    first commit
    install vite and react dependencies. Set folders structure and add Header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+export default function Header() {
+  return (
+    <div id="header">
+      <div className="left-section">
+        <h1>Game Name</h1>
+        <p>Game description - How to play</p>
+      </div>
+      <div className="right-section">
+        <p className="score">Score: xxx</p>
+        <p className="score">Best Score: xxx</p>
+      </div>
+    </div>
+  );
+}
